38509dfdc9e1567395880b418ebb43015f1f16af
Tomás Longeri <tlongeri@google.com>
2026-08-18 16:45:50 -0700

    [Mosaic] Rename TiledLayoutAttr::getDefaultTileStrides to getContiguousTileStrides
    Reverts 8537a319505e185323ecb0b761941faab81ecb98
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/125038 from somtri:fix-124974-blocklstm-seq-len-max-bounds caa875b0a0f4eac9dfa0a97df9b85fb6597a84eb
    PiperOrigin-RevId: 966876309

--- third_party/xla/xla/mosaic/dialect/tpu/tpu_dialect.cc
+++ third_party/xla/xla/mosaic/dialect/tpu/tpu_dialect.cc
@@ -396,7 +396,7 @@ FailureOr<SmallVector<int64_t>> getExpandedShape(
 }
 }  // namespace
 
-SmallVector<int64_t> TiledLayoutAttr::getDefaultTileStrides(
+SmallVector<int64_t> TiledLayoutAttr::getContiguousTileStrides(
     const ArrayRef<xla::Tile> tiles, const ArrayRef<int64_t> shape) {
   SmallVector<int64_t> strides(shape.size());
   int64_t stride = 1;
@@ -421,7 +421,7 @@ TiledLayoutAttr TiledLayoutAttr::getContiguous(MLIRContext* context,
                                                ArrayRef<xla::Tile> tiles,
                                                ArrayRef<int64_t> shape) {
   return TiledLayoutAttr::get(
-      context, tiles, TiledLayoutAttr::getDefaultTileStrides(tiles, shape));
+      context, tiles, TiledLayoutAttr::getContiguousTileStrides(tiles, shape));
 }
 
 int64_t TiledLayoutAttr::getNumTrailingDimsWithContiguousTiles(
